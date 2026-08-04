e6f39b4fa76c4d890627a06b1cfc81c531b06e11
Sergio Stulzer <sagacitas.assessoria@gmail.com>
2026-08-04 09:24:50 -0300

    chore: remove dados locais, scripts scratch e dumps do rastreamento

--- fix_fk.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE public.empresas_fornecedores ADD CONSTRAINT empresas_fornecedores_contrato_id_fkey FOREIGN KEY (contrato_id) REFERENCES public.empresa_contratante(contrato_id) ON DELETE CASCADE;
